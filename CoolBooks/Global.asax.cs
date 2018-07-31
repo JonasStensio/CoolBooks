4c2d51e907490e0fbc7b0653a1337b62ad36daeb
wael henawe <wael.henawe@outlook.com>
2018-07-31 12:45:54 +0200

    commit

--- CoolBooks/Global.asax.cs
+++ CoolBooks/Global.asax.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -19,6 +17,5 @@ namespace CoolBooks
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-       
     }
 }
