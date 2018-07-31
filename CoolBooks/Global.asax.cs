2bbd0c789b4e9d0afd3e366bc597d8b627e1d715
adam bergholtz <a.bergholtz@hotmail.com>
2018-07-31 14:37:13 +0200

    json isbn

--- CoolBooks/Global.asax.cs
+++ CoolBooks/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CoolBooks.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -14,11 +17,28 @@ namespace CoolBooks
     {
         protected void Application_Start()
         {
+            SearchBook("9780547928197");
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);        
         }
-       
+
+        public static string SearchBook(string isbn)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:49905");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn).Result;
+
+            string jsonData = response.Content.ReadAsStringAsync().Result;
+
+            var res = JsonConvert.DeserializeObject<Books>(jsonData);
+            Debug.WriteLine(jsonData);
+            return jsonData; // return book;
+            
+        }
+
+
     }
 }
